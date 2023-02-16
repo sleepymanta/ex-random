cafb803accb5e992edc1474db68010b4a92bf2f7
sleepymanta <124329693+sleepymanta@users.noreply.github.com>
2023-02-16 17:22:13 +0700

    Add files via upload

---
+++ app01.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	mrand "math/rand"
+	"time"
+)
+
+func main() {
+	// Random Byte
+	sliceRand := make([]byte, 8)
+	rand.Read(sliceRand)
+	fmt.Printf("Byte Random: % x\n", sliceRand)
+	// Byte Random: 72 04 3a 54 ca 55 e3 69
+
+	// Random Value 1 - 100
+	mrand.Seed(time.Now().UnixNano())
+	fmt.Printf("Math Random: %v\n", mrand.Intn(101-1)+1)
+	// Math Random: 45
+}
