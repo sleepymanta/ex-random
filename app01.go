bc83d9eb00e59f641360f034ed8a4c3cb40c2461
sleepymanta <124329693+sleepymanta@users.noreply.github.com>
2023-02-17 14:51:19 +0700

    Add files via upload

--- app01.go
+++ app01.go
@@ -3,8 +3,9 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"math"
+	"math/big"
 	mrand "math/rand"
-	"time"
 )
 
 func main() {
@@ -14,8 +15,16 @@ func main() {
 	fmt.Printf("Byte Random: % x\n", sliceRand)
 	// Byte Random: 72 04 3a 54 ca 55 e3 69
 
-	// Random Value 1 - 100
-	mrand.Seed(time.Now().UnixNano())
-	fmt.Printf("Math Random: %d\n", mrand.Intn(101-1)+1)
-	// Math Random: 45
+	// Random BigInt
+	seedInt64, _ := rand.Int(rand.Reader, big.NewInt(int64(math.MaxInt64)))
+
+	// Math Random Value 1 - 100 and 0 - 99
+	// mathRandom := mrand.New(mrand.NewSource(time.Now().UnixNano()))
+	mathRandom := mrand.New(mrand.NewSource(seedInt64.Int64()))
+	fmt.Printf("Math Random: %d %d\n", mathRandom.Intn(101-1)+1, mathRandom.Intn(100))
+	// Math Random: 45 91
+
+	// DEPRECATED: Go 1.20
+	// mrand.Seed(seedInt64.Int64())
+	// fmt.Printf("Math Random: %d\n", mrand.Intn(101-1)+1)
 }
